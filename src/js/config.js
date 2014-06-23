2525bf1b42a5013f4f0f0b22eb25eb7211c4c004
root <info@vyew.com>
2014-06-23 13:29:42 -0400

    real config checkin. last one was wrong

--- src/js/config.js
+++ src/js/config.js
@@ -1,10 +1,19 @@
 
+// config file for angular
+
 window.cfg={
-	hostAndPort:function(){
-		return 'http://'+window.location.host;
-		}
-	,host: function(){
-		return 'http://'+window.location.host.replace(/:[0-9]+$/, '');
-		}
-	};
+    hostAndPort:function(){
+        return 'http://'+window.location.host;
+        }
+    ,host: function(){
+        return 'http://'+window.location.host.replace(/:[0-9]+$/, '');
+        }
+    ,apiBaseUrl: function(){
+            var h=this.host()||'';
+            if(h.match(/app.arbor/)) return this.host()+'/api/v2.0';
+            else return 'http://app.arborplus.com/api/v2.0';
+                // todo - above should point at dev.arborplus, as soon as we get the subdomain up
+        }
+    };
+
 

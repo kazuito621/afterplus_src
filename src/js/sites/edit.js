e65ebf0930e97bf0b6a21d4101d5750d1e7331be
root <info@vyew.com>
2014-06-25 05:13:40 -0400

    minor changes

--- src/js/sites/edit.js
+++ src/js/sites/edit.js
@@ -8,15 +8,4 @@ var EditSiteCtrl = app.controller('EditSiteCtrl',
 		s.siteID;
 		s.site;
 		s.site_cachebuster='?ts='+moment().unix();
-
-	s.init = function() {
-		return
-	}
-
-	var pre_init = function() {
-		if($route.current.params.stateID==myStateID) s.init();
-	}
-	s.$on('$locationChangeSuccess', pre_init);
-	pre_init();
-
 }]);

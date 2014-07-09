d0c7b4879aa642b320adf1a1244e9eb3ff37b68d
root <info@vyew.com>
2014-07-09 07:11:49 -0400

    move service into its own file

--- src/js/trees/trees.js
+++ src/js/trees/trees.js
@@ -822,44 +822,5 @@ var TreesCtrl = app.controller('TreesCtrl',
 }]);	// }}} TreesCtrl
 
 
-/**
-	Service for updates to various site models.
-**/
-app.service('SiteModelUpdateService', 
-	[
-	function() {
-	
-	var site = {};
-	var sites = {};
-	var reportSite = {};
-
-	this.init=function(){}
-
-	this.updateSiteModels=function(mysite){
-		if (!$.isEmptyObject(this.sites)){
-			this.site = mysite;
-			$.each(this.sites, function(index, curSite){
-				if (mysite.siteID == curSite.siteID) {
-					curSite.siteName = mysite.siteName;
-					return false; //break
-				}
-			});
-			this.updateReportSiteModel();
-		}
-	}
-	this.setSites=function(sites){
-		this.sites = sites;
-	}
-	this.init();
-
-	this.setReportSiteModel=function(report){
-		this.reportSite = report;
-	}
-
-	this.updateReportSiteModel=function(){
-		this.reportSite.siteName = this.site.siteName;
-	}
-
-}]);
 
 

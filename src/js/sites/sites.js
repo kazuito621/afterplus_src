c02e9ea2e9697e4fb72b7b7d5870ce4d9438143b
Nikita Muratov <muratov.nikita@gmail.com>
2014-07-27 20:50:27 +0400

    Sites.js code reformating

--- src/js/sites/sites.js
+++ src/js/sites/sites.js
@@ -1,99 +1,98 @@
-'use strict';
+var SitesCtrl = app.controller('SitesCtrl',
+    ['$scope', '$route', '$modal', '$location', 'SiteModelUpdateService', 'Api',
+        function ($scope, $route, $modal, $location, SiteModelUpdateService, Api) {
+            'use strict';
+            var s = window.scs = $scope;
+            var myStateID = 'sites';
+            s.mode = '';
+            s.type = 'site';
+            s.newSite = {clientID: ''};
+            s.items = {};
 
-var SitesCtrl = app.controller('SitesCtrl', 
-['$scope', '$route', '$modal', '$location', 'SiteModelUpdateService', 'Api',
-function ($scope, $route, $modal, $location, SiteModelUpdateService, Api) {
-	var s=window.scs=$scope
-		,myStateID='sites'
-		,mode=''
-		,type='site'
-	s.newSite={clientID:''};
-	s.items = {};
+            var init = function () {
+                return;	// using initData list for now... may need this later if we want more data
+            };
 
-	var init = function() {
-		return;	// using initData list for now... may need this later if we want more data
-	};
+            var pre_init = function () {
+                if ($route.current.params.stateID === myStateID) {
+                    init();
+                }
+            };
 
-	s. select = function(siteID){
-		return;
-		//todo - make this work... there is a bug here
-		s.selected.siteID=siteID;
-		$location.url('/trees');
-	};
+            // Pre-fetch an external template populated with a custom scope
+            var siteEditModal = $modal({scope: $scope, template: 'js/sites/edit.tpl.html', show: false});
 
-	s.saveNewSite = function() {
-		if(!s.newSite.clientID) {
-			return s.setAlert('Choose a client for the new property',{type:'d'});
-		} else {
-            Api.saveNewSite(s.newSite).then(function(data) {});
-			siteEditModal.hide();
-			// is this needed? this may have been in evrens 07 branch
-			// if($route.current.params.stateID=='sites') {
-			Api.refreshInitData();
-		}
-	};
+            s.select = function (siteID) {
+                return;
+                //todo - make this work... there is a bug here
+                s.selected.siteID = siteID;
+                $location.url('/trees');
+            };
 
-	s.saveExistingSite = function() {
-		var obj=s.site;
-		var that=this;
-		obj.post().then(function(){
-			Api.refreshInitData();
-		});
-		// Update all other sites models, eg. the sites dropdown on the trees report
-		SiteModelUpdateService.updateSiteModels(obj);
-		siteEditModal.hide();
-	};
-	
-	var pre_init = function() {
-		if ($route.current.params.stateID === myStateID) {
-            init();
-        }
-	};
-	s.$on('$locationChangeSuccess', pre_init);
-	pre_init();
+            s.saveNewSite = function () {
+                if (!s.newSite.clientID) {
+                    return s.setAlert('Choose a client for the new property', {type: 'd'});
+                }
 
-	// Pre-fetch an external template populated with a custom scope
-	var siteEditModal = $modal({scope: $scope, template: '/js/sites/edit.tpl.html', show: false});
+                Api.saveNewSite(s.newSite).then(function (data) {});
+                siteEditModal.hide();
+                // is this needed? this may have been in evrens 07 branch
+                // if($route.current.params.stateID=='sites') {
+                Api.refreshInitData();
+            };
 
-	s.newSiteModalOpen = function (siteID) {
-		s.site={};
-		s.newSite={clientID:''};
-		s.mode='new';
-		siteEditModal.show();
-	};
+            s.saveExistingSite = function () {
+                var obj = s.site;
 
-	s.existingSiteModalOpen = function (siteID) {
-		s.site = {};
-        Api.updateSite(siteID)
-			.then(function(data){
-				s.site = data;
-		    });
-		s.mode = 'edit';
-		siteEditModal.show();
-	};
+                obj.post().then(function () {
+                    Api.refreshInitData();
+                });
+                // Update all other sites models, eg. the sites dropdown on the trees report
+                SiteModelUpdateService.updateSiteModels(obj);
+                siteEditModal.hide();
+            };
 
-	s.deleteItems = function (itemID) {
-		Api.removeSiteById(itemID).then(function(data) {
-			Api.refreshInitData();
-		});
-		Api.refreshInitData();
-	};
+            s.newSiteModalOpen = function (siteID) {
+                s.site = {};
+                s.newSite = {clientID: ''};
+                s.mode = 'new';
+                siteEditModal.show();
+            };
 
-	s.queueOrDequeueItemForDelete = function(itemID) {
-		if (!s.isSelected(itemID)) {
-			s.items[itemID] = '1';
-		} else {
-			delete s.items[itemID];
-		}
-		s.type = 'site';
-	};
+            s.existingSiteModalOpen = function (siteID) {
+                s.site = {};
+                Api.updateSite(siteID)
+                    .then(function (data) {
+                        s.site = data;
+                    });
+                s.mode = 'edit';
+                siteEditModal.show();
+            };
 
-	s.isSelected = function(itemID) {
-		if (s.items[itemID] === 1) {
-			return true;
-		} else {
-			return false;
-		}
-	};
+            s.deleteItems = function (itemID) {
+                Api.removeSiteById(itemID).then(function (data) {
+                    Api.refreshInitData();
+                });
+                Api.refreshInitData();
+            };
 
-}]);
+            s.queueOrDequeueItemForDelete = function (itemID) {
+                if (!s.isSelected(itemID)) {
+                    s.items[itemID] = '1';
+                } else {
+                    delete s.items[itemID];
+                }
+                s.type = 'site';
+            };
+
+            s.isSelected = function (itemID) {
+                if (s.items[itemID] === 1) {
+                    return true;
+                }
+
+                return false;
+            };
+
+            s.$on('$locationChangeSuccess', pre_init);
+            pre_init();
+        }]);

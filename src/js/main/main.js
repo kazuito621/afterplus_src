16dfb301e683dd7ce61b2bb3be0851a8f4dcaac5
Nikita Muratov <muratov.nikita@gmail.com>
2014-08-06 21:34:13 +0400

    Adding Auth.isAtleast to main scope

--- src/js/main/main.js
+++ src/js/main/main.js
@@ -17,6 +17,7 @@ function ($scope, Rest, $routeParams, $route, $alert, storage, $timeout, $rootSc
 		,isSignedIn: angular.bind(Auth, Auth.isSignedIn)
 		,getLoginName: angular.bind(Auth, Auth.getLoginName)
 		,signOut: angular.bind(Auth, Auth.signOut)
+        ,isAtleast: angular.bind(Auth, Auth.isAtleast)
 	}
 
     s.isActiveTab = function (name) {

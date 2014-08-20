e53e886e1e6251efb554491b1e581299d2489262
Nikita Muratov <muratov.nikita@gmail.com>
2014-08-20 14:32:42 +0400

    Site edit modal directive update

--- src/js/common/directives/siteEditModal.js
+++ src/js/common/directives/siteEditModal.js
@@ -1,10 +1,24 @@
-app.directive('siteEditModal', function ($modal, SiteModelUpdateService, Api) {
+app.directive('siteEditModal', function ($modal, SiteModelUpdateService, Api, $window) {
     'use strict';
 
     var linker = function (scope, el) {
         var modal;
         var newSite = {clientID: ''};
-        var newContact = {};
+        var newContact = { role: 'customer'};
+
+        var separateUsers = function (users) {
+            var res = { contacts: [], reps: []};
+
+            angular.forEach(users, function (user) {
+                if (user.role === 'customer') {
+                    res.contacts.push(user);
+                } else if (user.role === 'sales' || user.role === 'inventory') {
+                    res.reps.push(user);
+                }
+            });
+
+            return res;
+        };
 
         scope.mode = '';
         scope.newContact = angular.copy(newContact);
@@ -20,6 +34,13 @@ app.directive('siteEditModal', function ($modal, SiteModelUpdateService, Api) {
                 scope.mode = 'edit';
                 Api.updateSite(id).then(function (data) {
                     scope.site = data;
+                    Api.getSiteUsers(id).then(function (data) {
+                        var separatedUsers = separateUsers(data);
+                        scope.contacts = separatedUsers.contacts;
+                        scope.reps = separatedUsers.reps;
+
+                        console.log('Edit site', scope.site);
+                    });
                 });
             } else {
                 scope.mode = 'new';
@@ -36,13 +57,11 @@ app.directive('siteEditModal', function ($modal, SiteModelUpdateService, Api) {
             }
 
             if (scope.site.id) {
-                var obj = scope.site;
-
-                obj.post().then(function () {
+                scope.site.post().then(function () {
                     Api.refreshInitData();
                 });
                 // Update all other sites models, eg. the sites dropdown on the trees report
-                SiteModelUpdateService.updateSiteModels(obj);
+                SiteModelUpdateService.updateSiteModels(scope.site);
             } else {
                 Api.saveNewSite(scope.site).then(function () {
                     Api.refreshInitData();
@@ -53,8 +72,44 @@ app.directive('siteEditModal', function ($modal, SiteModelUpdateService, Api) {
         };
 
         scope.userSelect = function (user) {
-            scope.newContact.firstName = user.fName;
-            scope.newContact.lastName = user.lName;
+            scope.newContact.fName = user.fName;
+            scope.newContact.lName = user.lName;
+            scope.newContact.userID = user.userID;
+        };
+
+        scope.addNewSiteContact = function (event) {
+            event.preventDefault();
+            event.stopPropagation();
+
+            scope.showAddNewSiteContact = false;
+
+            var tmp = angular.copy(scope.newContact);
+            var user = { role: tmp.role };
+
+            if (!tmp.userID) {
+                user.fName = tmp.fName;
+                user.lName = tmp.lName;
+            } else {
+                user.userID = tmp.userID;
+            }
+
+            console.log('Add new site contact', user);
+            Api.userSite.assign(scope.siteId, scope.newContact).then(function () {
+                scope.newContact = angular.copy(newContact);
+            });
+        };
+
+        scope.unassign = function (userID, email) {
+            if ($window.confirm('User ' + email + ' will be unassigned. Please confirm.')) {
+                Api.userSite.unassign(scope.siteId, userID);
+                scope.contacts = _.filter(scope.contacts, function (contact) {
+                    return contact.userID !== userID;
+                });
+
+                scope.reps = _.filter(scope.reps, function (rep) {
+                    return rep.userID !== userID;
+                });
+            }
         };
 
         var init = function () {

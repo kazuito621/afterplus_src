7e9618deeac9b925b56b67bc4d187ae2b7d83fcd
Nikita Muratov <muratov.nikita@gmail.com>
2014-10-25 09:07:11 +0200

    Hazards filter update

--- src/js/common/directives/filters/filter.hazards/filter.hazards.js
+++ src/js/common/directives/filters/filter.hazards/filter.hazards.js
@@ -2,6 +2,45 @@ app.directive('filterHazards',
     [function () {
         'use strict';
 
+        var linker = function (scope) {
+            scope.hazards = [
+                {
+                    key: 'building',
+                    value: 'building',
+                    name: 'Building',
+                    class: 'fa fa-building _red'
+                },
+                {
+                    key: 'caDamagePotential',
+                    value: 'yes',
+                    name: 'Hardscape Damage Potential',
+                    class: 'fa fa-warning _orange'
+                },
+                {
+                    key: 'caDamage',
+                    value: 'yes',
+                    name: 'Hardscape Damage',
+                    class: 'fa fa-warning _red'
+                },
+                {
+                    key: 'powerline',
+                    value: 'yes',
+                    name: 'Powerline',
+                    class: 'fa fa-bolt _red'
+                }
+            ];
+
+            scope.isShown = function () {
+                var res = false;
+
+                angular.forEach(scope.hazards, function (h) {
+                    res = res || scope.tfsCounts[h.key];
+                });
+
+                return res;
+            };
+        };
+
         return {
             restrict: 'EA',
             replace: true,
@@ -11,6 +50,7 @@ app.directive('filterHazards',
                 tfsCounts: '=',         //    TFSdata.filterTypeCounts
                 initDataHazards: '=',   //    initdata.filters.hazards
                 onFilterChange: '&'    //    onFilterChange(filterType, ID, value)
-            }
+            },
+            link: linker
         };
     }]);

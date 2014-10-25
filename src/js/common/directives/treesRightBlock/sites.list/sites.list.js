93bcb96adc4361716c943b4641f3ce197b25eff1
Nikita Muratov <muratov.nikita@gmail.com>
2014-10-25 10:36:50 +0200

    Sites list directive ( transparent ) first version

---
+++ src/js/common/directives/treesRightBlock/sites.list/sites.list.js
@@ -0,0 +1,11 @@
+app.directive('sitesList',
+    [function () {
+        'use strict';
+
+        return {
+            restrict: 'EA',
+            replace: true,
+            transclude: false,
+            templateUrl: 'js/common/directives/treesRightBlock/sites.list/sites.list.tpl.html'
+        };
+    }]);

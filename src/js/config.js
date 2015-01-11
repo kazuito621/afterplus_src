486fcae0dd546b5f3ccd58321beded25a7d97c32
root <info@vyew.com>
2015-01-10 16:33:48 -0800

    update logo entity 1 to generic arborplus

--- src/js/config.js
+++ src/js/config.js
@@ -21,7 +21,10 @@ window.cfg={
             var h=this.host()||'';
 			if(h.match(/app.aplus/)) return 2;
 			if(h.match(/joseph/)) return 3;
-			if(h.match(/woodie/)) return 4;
+			if(h.match(/woodie|woody/)) return 4;
+			if(h.match(/hendricks/)) return 7;
+			if(h.match(/shreiner/)) return 7;
+			if(h.match(/acme/)) return 6;
 			return 1;	// default to dev.aplustree
 		}
 	,getEntity: function(){

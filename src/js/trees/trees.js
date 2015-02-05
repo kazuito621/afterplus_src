kazuito621/afterplus_src

commit c197e44058bad237ac37bf03961332f5743a49db

Map does not load some times issue - fixed.

src/js/trees/trees.js
@@ -367,6 +367,14 @@ var TreesCtrl = app.controller('TreesCtrl',
             //    if (s.data.mode() == 'trees') ReportService.loadRecent();
             //});
 
+            //Wil be fired when user select report id from recent drop down.
+            s.$on('OnLoadReportEvent', function (evt, data) {
+                if (data.siteID == undefined || data.siteID == "")
+                    return;
+                                
+                s.onSelectSiteID(data.siteID);
+            });
+
             // When the trees[] array changes because of a filter event... update the ui.
             // This also may be triggered when site dropdown is selected (via getTreeListings())
             s.$on('onTreeFilterUpdate', function (evt, trees) {
@@ -1736,7 +1744,7 @@ var TreesCtrl = app.controller('TreesCtrl',
             //Broadcast is not occuring.
             s.$on('onInitData', function (e, data) {
                 console.log('On init data in trees');
-                
+
                 if (s.data.mode() === 'trees' && (!gMap || !gMap.j || gMap.j.id !== 'treeMap')) {
                     console.log('Map not initialized in $onInitData event');
                     initMap().then(function () {

src/js/trees/report.js
@@ -1,7 +1,7 @@
 var ReportCtrl = app.controller(
     'ReportCtrl',
-    ['$scope', 'Api', '$route', '$timeout', 'ReportService', '$location', '$anchorScroll', 'Auth','$modal','$q',
-        function ($scope, Api, $route, $timeout, ReportService, $location, $anchorScroll, Auth,$modal,$q) {
+    ['$scope', 'Api', '$route', '$timeout', 'ReportService', '$location', '$anchorScroll', 'Auth','$modal','$q','$rootScope',
+        function ($scope, Api, $route, $timeout, ReportService, $location, $anchorScroll, Auth, $modal, $q, $rootScope) {
             'use strict';
 
             // local and scoped vars
@@ -19,6 +19,7 @@ var ReportCtrl = app.controller(
             s.siteOfReport={};
             var reportBackUp;
             var changedItems = [];
+            
 
 			s.afiliations=cfg.getEntity().afiliations || '';
 			if(s.afiliations)s.afiliations=s.afiliations.split(',');
@@ -72,8 +73,12 @@ var ReportCtrl = app.controller(
 
             // When a new report is loaded, bind it to this scope
             s.$on('onLoadReport', function (evt, rpt) {
-                s.report = rpt;
-                s.siteOfReport={}
+                s.report = rpt;                
+                
+                //Use to load site on basis of recent selected report in tree.js
+                $rootScope.$broadcast('OnLoadReportEvent', { siteID: rpt.siteID });
+
+                s.siteOfReport = {}
                 s.report.customers=[]
                 // set email links
                 if (rpt.emailLogs) {

commit b0ebf5d24f4f3aaeddd53410fc8721a569208de1

FIX: a bug introduced by c197e44058bad237ac37bf03961332f5743a49db where the map broke when viewing customer estimate view

src/js/trees/trees.js
@@ -323,8 +323,12 @@ var TreesCtrl = app.controller('TreesCtrl',
             s.onSelectSiteID = function (id) {
                 s.selected.siteID = id;
                 ReportService.setSiteID(id);
+
+                if (s.data.mode() != 'trees') return;
+
+                ReportService.loadRecent();
+
                 if (id && id > 0) {
-                    console.log('On select site id', id);
                     if (s.data.mode() == 'trees') {
                         ReportService.loadRecent();
                     }
@@ -370,6 +374,8 @@ var TreesCtrl = app.controller('TreesCtrl',
             //    if (s.data.mode() == 'trees') ReportService.loadRecent();
             //});
 
+
+
             //Wil be fired when user select report id from recent drop down.
             s.$on('OnLoadReportEvent', function (evt, data) {
                 if (data.siteID == undefined || data.siteID == "")
@@ -377,6 +383,8 @@ var TreesCtrl = app.controller('TreesCtrl',
                                 
                 s.onSelectSiteID(data.siteID);
             });
+
+
 
             // When the trees[] array changes because of a filter event... update the ui.
             // This also may be triggered when site dropdown is selected (via getTreeListings())
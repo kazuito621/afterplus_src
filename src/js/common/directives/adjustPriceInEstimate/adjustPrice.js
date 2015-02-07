cb216237c2128afcc85556eb58953d180fb7ef6b
Imdadul Huq <imdadul.huq3@gmail.com>
2015-02-07 07:59:27 +0600

    Adjust price fix + change

--- src/js/common/directives/adjustPriceInEstimate/adjustPrice.js
+++ src/js/common/directives/adjustPriceInEstimate/adjustPrice.js
@@ -2,8 +2,8 @@
  * Created by usert on 17-Jan-15.
  */
 app.directive('adjustPrice',
-    ['$popover','ReportService',
-        function ($popover,ReportService) {
+    ['$popover','ReportService','$interval',
+        function ($popover,ReportService,$interval) {
             return {
                 restrict: 'EA',
                 link: function (scope, el, attrs) {
@@ -39,10 +39,22 @@ app.directive('adjustPrice',
                             return;
                         }
                     };
-                    scope.change=function(){
-                        var n=this.adjustPercentage;
+                    scope.change=function(newPV){
+                        var n;
+                        if(newPV!=undefined){
+                            n=newPV;
+                            $('#adjustPercentage').val(scope.popover.$scope.adjustPercentage); // sometime this ui field does not updates!!
+                        }
+                        else {
+                            n=this.adjustPercentage;
+                        }
                         var doRoundPrice=this.doRoundPrice;
                         if(isNaN(n)==false && n>-100 && n<100 ){
+                            scope.popover.$scope.adjustPercentage=parseFloat(n);
+                            this.adjustPercentage=parseFloat(n);
+                            scope.adjustPercentage=parseFloat(n);
+
+
                             scope.newTotal=0;
                             _.each(scope.$parent.report.items,function(item){
                                 var v=(parseFloat(item.price)*(1+n/100));
@@ -59,7 +71,7 @@ app.directive('adjustPrice',
                                     scope.newTotal=scope.newTotal+ parseFloat(v.toFixed(2));
                                 }
                             });
-                            scope.newTotal=parseFloat(scope.newTotal.toFixed(2))
+                            scope.newTotal=parseFloat(scope.newTotal.toFixed(2));
                         }
                         else {
                             scope.newTotal="N/A";
@@ -72,7 +84,33 @@ app.directive('adjustPrice',
                             scope.popover.$scope.adjustPercentage=0;
                             scope.popover.hide();
                         }
+                    };
+
+                    var promise
+                    var changePercentageValue=function(dir){
+                        var newPV;
+                        if (dir === "up")
+                            scope.popover.$scope.adjustPercentage = parseFloat(scope.popover.$scope.adjustPercentage) + 1;
+                        else
+                            scope.popover.$scope.adjustPercentage = parseFloat(scope.popover.$scope.adjustPercentage) - 1;
+                        newPV=scope.popover.$scope.adjustPercentage;
+                        scope.change(newPV);
                     }
+                    scope.mouseDown = function(dir) {
+                        if(isNaN(this.adjustPercentage)) return;
+                        scope.doRoundPrice=this.doRoundPrice;
+                        changePercentageValue(dir);
+                        promise = $interval(function () { // Change for on hold mouse.
+                            changePercentageValue(dir);
+                            this.adjustPercentage+=1;
+                        }, 120);
+
+                    };
+
+                    scope.mouseUp = function () {
+                        $interval.cancel(promise);
+                    };
+
                     $(el).click(function () {
                         scope.adjustPercentage=0;
                         scope.newTotal=angular.copy(scope.$parent.report.total.items);

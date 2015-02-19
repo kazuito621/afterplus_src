c992342b45bb04c8ce1feb0ba66c197705c394e5
Stan Makarenko <gosatin@gmail.com>
2015-02-18 22:23:18 -0800

    Added checks for all 4 main pages + login. Run 'npm install; grunt update' to install modules and perform init config for protractor. e2e test is part of grunt test now (or run 'grunt e2e')

--- test/e2e/smoke.js
+++ test/e2e/smoke.js
@@ -1,26 +1,47 @@
 describe('app', function() {
   beforeEach(function() {
-    browser.manage().timeouts().pageLoadTimeout(40000);
     browser.manage().timeouts().implicitlyWait(25000);
     browser.get('http://0.0.0.0:9000');
   });
-  it('should have a title', function() {
+
+  xit('should have a title', function() {
     expect(browser.getTitle()).toEqual('ArborPlus Tree Management');
   });
 
-  iit('should let user to login', function() {
+  it('should let user to login', function() {
+    // do login
     var email = element(by.model('login.email'));
     email.sendKeys('timhon@gmail.com');
     var password = element(by.model('login.pswd'));
     password.sendKeys('asdf');
     var signInBtn = element(by.css('.btn[type=submit]'));
     signInBtn.click();
-    console.log('test');
+
+    // check Trees page
     var speciesChkBoxLocator = by.css('input#checkBox_filter_species_273');
+    // wait for login action to complete. we know it's done when there is a checkbox available
     browser.wait(function() {
       return browser.driver.isElementPresent(speciesChkBoxLocator);
-    }, 8000);
+    });
     var speciesChkBox = element(speciesChkBoxLocator).click();
     expect(speciesChkBox.isSelected()).toBe(true);
+
+    //check estimates page
+    element(by.css('.fa-dollar')).click();
+    expect(browser.driver.isElementPresent(by.css('h1.pull-left'))).toBe(true);
+    expect(element(by.css('h1.pull-left')).getText()).toEqual('Estimates');
+
+    //check properties page
+    element(by.css('.fa-home')).click();
+    expect(browser.driver.isElementPresent(by.css('h1.pull-left'))).toBe(true);
+    expect(element(by.css('h1.pull-left')).getText()).toEqual('Properties');
+
+    //check clients page
+    element(by.css('.fa-user')).click();
+    browser.wait(function() {
+      return browser.driver.isElementPresent(by.css('.fa-plus'));
+    });
+    expect(browser.driver.isElementPresent(by.css('h1.pull-left'))).toBe(true);
+    expect(element(by.css('h1.pull-left')).getText()).toEqual('Clients');
   });
 });

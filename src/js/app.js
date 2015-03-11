a1b5aa9897d8173b818f1902b8595b3d44ae9e55
Stan Makarenko <gosatin@gmail.com>
2015-03-10 23:24:26 -0700

    Added Google Analytics code to automatically track pageviews and programatically track events

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,9 @@
 var app = angular.module('arborPlusApp', 
 	['ngRoute', 'restangular', 'arborPlusFilters', 'ngTable', 'angular-md5',
 	 'xeditable', 'ngSanitize', 'ngAnimate', 'mgcrea.ngStrap', 'angularLocalStorage', 'checklist-model',
-     'ngCkeditor', 'pasvaz.bindonce', 'infinite-scroll', 'ngTagsInput', 'templates-main', 'GoogleMapsInitializer']);
+         'ngCkeditor', 'pasvaz.bindonce', 'infinite-scroll', 'ngTagsInput', 'templates-main', 'GoogleMapsInitializer',
+         'angulartics', 'angulartics.google.analytics'
+        ]);
 
 app.config(['$routeProvider', '$locationProvider',
 	function ($routeProvider, $locationProvider) {

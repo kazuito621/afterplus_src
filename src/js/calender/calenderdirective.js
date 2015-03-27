02a39341bcb41391e280c91c3e61c99c3e287aff
Gurnoor <Gurnoor@ameotech.com>
2015-03-27 20:04:32 +0530

    When I drag event/jobs from calender  date (say 03-27-2015) to  another date (03-29-2015) Then event/jobs display twice on date.( It happened in only first time). (FIXED)

--- src/js/calender/calenderdirective.js
+++ src/js/calender/calenderdirective.js
@@ -21,15 +21,14 @@
 
             Api.getRecentReports({ siteID: search.siteID }).then(function (data) {
                 angular.forEach(data, function (field) {
-                    if (field.status == "paid") {
-                        $scope.UnscheduledJobs.push(
+                    if (field.status == "paid" && field.name != null) {
+                        $scope.ScheduledJobs.push(
                             {
                                 "title": field.name,
                                 "start": "2015-03-02T16:00:00",
                                 "price": "," + field.total_price
 
                             });
-
 
                         var bindexternalevents = setTimeout(function () {
                             var externalevents = $element.find(".fc-event");
@@ -49,19 +48,17 @@
                                     revertDuration: 0  //  original position after the drag
                                 });
                             });
-                        }, 1000);
+                        }, 4490);
                     }
                     if (field.status == "draft" && field.name != null) {
 
-                        $scope.ScheduledJobs.push(
+                        $scope.UnscheduledJobs.push(
                             {
                                 "title": field.name.trim(),
                                 "start": "2015-03-02",
                                 "price": "," + field.total_price
 
                             });
-
-
                     }
 
                     // clearInterval(bindexternalevents);
@@ -83,9 +80,8 @@
                     eventLimit: $scope.eventfullcalender,
                     startEditable: true,
                     events: $scope.ScheduledJobs,
-                    //selectable: true,
                     select: function (start, end) {
-                      
+
                         var title = prompt('Event Title:');
                         var eventData;
                         if (title) {
@@ -99,7 +95,7 @@
                         $('#calendar').fullCalendar('unselect');
                     },
                     drop: function (el, eventStart, ev, ui) {
-                       
+
                         $('.fc-title br').remove();
                         console.log(el._i);
                         console.log(el._d);
@@ -112,15 +108,13 @@
 
                     },
                     eventDrop: function (event, delta, revertFunc) {
-                       
-                        $('.fc-title br').remove();
-
-                        console.log(event.start.format());
-                        console.log(event.title);
-                        console.log(event.start._i);
-                        console.log(event.start._d);
-                        //console.log(event.end);
-                        // console.log(event.work);
+                        setTimeout(function () {
+                            //console.log(event.start.format());
+                            //console.log(event.title);
+                            //console.log(event.start._i);
+                            //console.log(event.start._d);
+                        }, 5000);
+                        
                     },
                     updateEvent: function (event) {
                         console.log(event);
@@ -134,249 +128,41 @@
                         //$("#eventContent").dialog({ modal: true, title: data.title, width: 350 });
                     },
                     eventRender: function (event, element, view) {
+                        
                         $('.fc-title br').remove();
-                        if (event.title === "" || event.title === null) {
-                            var onMouseHoverJob = "angular.element(this).scope().onMouseHoverJob({0})".format(event.title);
-                            //var onMouseLeaveJob = "angular.element(this).scope().onMouseLeaveJob({0})".format(event.title);
-                            element.css('background-color', '#77DD77');
-                            element.find(".fc-content").append('<a href="#"  style="float:right;margin-top:-15px;0" onmouseover="{0}">'.format(onMouseHoverJob) + '<i class="glyphicon glyphicon-exclamation-sign" style="color:red;" title="No foreman assigned to this job"></i></a>');
-                            // element.find(".fc-content").append('<a style="float:right;margin-top:-15px;" class="glyphicon glyphicon-exclamation-sign" id="tooltipCalender"    title="No foreman assigned to this job" onmouseover="{0}" ></a>'.format(onMouseHoverJob) + '<div id="tooltip" style="float:left;" class="hide">Hello Hi</div>');
-                            //element.find(".fc-content").append('<a style="float:right;margin-top:-15px;" class="glyphicon glyphicon-exclamation-sign tooltipCalender" title="No foreman assigned to this job"  ></a><div id="tooltip" class="hide">Hello Hi</div>');
-                        }
-                        else {
-                            element.css('background-color', '#FFB347')
-                        }
+                      
+                            if (event.title === "" || event.title === null) {
+                                var onMouseHoverJob = "angular.element(this).scope().onMouseHoverJob({0})".format(event.title);
+
+                                element.css('background-color', '#77DD77');
+                                element.find(".fc-content").append('<a href="#"  style="float:right;margin-top:-15px;0" onmouseover="{0}">'.format(onMouseHoverJob) + '<i class="glyphicon glyphicon-exclamation-sign" style="color:red;" title="No foreman assigned to this job"></i></a>');
+                            }
+                            else {
+                                element.css('background-color', '#FFB347')
+                            }
+                      
+                      
+                       
                     },
                     eventResize: function (event, delta, revertFunc, jsEvent, ui, view) {
-                        //angular.element("div").find(fc-title
-                        //var a = angular.element(this).find('br').remove();
-
+                     
                         var html = $(view.el[0]).find(".fc-title").html();
                         html = html.replace("<br/>", "");
                         html = html.replace("<br>", "");
                         $(".fc-title").html(html);
-                        //$('div .fc-content .fc-title br').remove();
-                    
-                    },
+                       
 
+                    },
                 });
             });
 
             $scope.onMouseHoverJob = function () {
                 $("#tooltip").removeClass("hide").addClass("show");
-                //setTimeout(function () {
-                //    $("#tooltip").removeClass("show").addClass("hide");
-                //},500);
+               
             };
             $scope.onMouseLeaveJob = function () {
                 $("#tooltip").removeClass("show").addClass("hide");
             };
-
-
-            $scope.events = [
-				{
-				    title: 'All Day Event',
-				    start: '2015-02-01',
-				    description: 'Great Today is Holiday'
-				},
-				{
-				    title: 'Long Event',
-				    start: '2015-02-07',
-				    end: '2015-02-10',
-				    description: 'Long Journey'
-				},
-				{
-				    id: 999,
-				    title: 'Repeating Event',
-				    start: '2015-03-09T16:00:00',
-				    description: ' Here I will repeat my work'
-				},
-				{
-				    id: 919,
-				    title: 'Repeating Event',
-				    start: '2015-03-16T16:00:00',
-				    description: 'Same Here'
-				},
-				{
-				    title: 'Conference',
-				    start: '2015-03-11',
-				    end: '2015-03-13',
-				    description: 'Today Sir, your meeting with MD of GSKC.So Be ready Zaeem'
-				},
-				{
-				    title: 'Meeting',
-				    start: '2015-03-12T10:30:00',
-				    end: '2015-03-12T12:30:00',
-				    description: 'Thats Great'
-				},
-				{
-				    title: 'Lunch',
-				    start: '2015-03-24',
-				    end: '2015-03-28',
-				    description: 'Guys Lunch Time'
-
-				},
-				{
-				    title: 'Meeting',
-				    start: '2015-03-12T14:30:00',
-				    description: 'Be ready Appointments'
-				},
-				{
-				    title: 'Happy Hour',
-				    start: '2015-03-12T17:30:00',
-				    description: 'Now You Happy'
-				},
-				{
-				    title: 'Dinner',
-				    start: '2015-03-12T20:00:00',
-				    description: 'Be ready Lunch Time'
-				},
-				{
-				    title: 'Birthday Party',
-				    start: '2015-03-13T07:00:00',
-				    description: 'Lets Rock'
-				},
-				{
-				    title: 'Click for Google',
-				    url: 'http://google.com/',
-				    start: '2015-03-28',
-				    description: 'World of Google.'
-				}
-            ];
-
-            $scope.job = [
-               {
-                   title: 'All Day Event',
-                   start: '2015-02-01',
-                   description: 'Great Today is Holiday'
-               },
-               {
-                   title: 'Long Event',
-                   start: '2015-02-07',
-                   end: '2015-02-10',
-                   description: 'Long Journey'
-               },
-               {
-                   id: 999,
-                   title: 'Repeating Event',
-                   start: '2015-03-09T16:00:00',
-                   description: ' Here I will repeat my work'
-               },
-               {
-                   id: 919,
-                   title: 'Repeating Event',
-                   start: '2015-03-16T16:00:00',
-                   description: 'Same Here'
-               },
-               {
-                   title: 'Conference',
-                   start: '2015-03-11',
-                   end: '2015-03-13',
-                   description: 'Today Sir, your meeting with MD of GSKC.So Be ready Zaeem'
-               },
-               {
-                   title: 'Meeting',
-                   start: '2015-03-12T10:30:00',
-                   end: '2015-03-12T12:30:00',
-                   description: 'Thats Great'
-               },
-               {
-                   title: 'Lunch',
-                   start: '2015-03-24',
-                   end: '2015-03-28',
-                   description: 'Guys Lunch Time'
-
-               },
-               {
-                   title: 'Meeting',
-                   start: '2015-03-12T14:30:00',
-                   description: 'Be ready Appointments'
-               },
-               {
-                   title: 'Happy Hour',
-                   start: '2015-03-12T17:30:00',
-                   description: 'Now You Happy'
-               },
-               {
-                   title: 'Dinner',
-                   start: '2015-03-12T20:00:00',
-                   description: 'Be ready Lunch Time'
-               },
-               {
-                   title: 'Birthday Party',
-                   start: '2015-03-13T07:00:00',
-                   description: 'Lets Rock'
-               },
-               {
-                   title: 'Click for Google',
-                   url: 'http://google.com/',
-                   start: '2015-03-28',
-                   description: 'World of Google.'
-               }
-            ];
-
-            /*var elm = $element.find("#calendar");
-             elm.fullCalendar({
-                 header: {
-                     left: 'prev,next today',
-                     center: 'title',
-                     right: 'month,agendaWeek,agendaDay',
-                 },
-                 //defaultDate: '2015-02-12',
-                 
-                 editable: $scope.editablefullcalender,     // Under calender events drag start on true and vice-versa.
-                 droppable: $scope.dropablefullcalender,
-                 eventLimit: $scope.eventfullcalender,
-                 startEditable: true,
-                 events: $.map($scope.UnscheduledJobs, function (item) {
-                     console.log($scope.UnscheduledJobs);
-                     var event = new Object();
-                     event.title = item.price;
-                     event.start = new Date(item.start);
-                     event.title = item.title;
-                     return event;
-                 }),
-                 //selectable: true,
-                 select: function (start, end) {
-                     var title = prompt('Event Title:');
-                     var eventData;
-                     if (title) {
-                         eventData = {
-                             title: title,
-                             start: start,
-                             end: end
-                         };
-                         $('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true
-                     }
-                     $('#calendar').fullCalendar('unselect');
-                 },
-                 drop: function (el, eventStart, ev, ui) {
-                     console.log(el._i);
-                     console.log(el._d);
-                     console.log(ev.helper[0].textContent);
-                     if ($('#drop-remove').is(':checked')) {
-                         // if so, remove the element from the "Draggable Events" list
-                         $(this).remove();
-                     }
-                 },
-                 eventDrop: function (el, eventStart, ev, ui) {
-                     console.log(el.title);
-                     console.log(el.start._i);
-                     console.log(el.start._d);
-                     console.log(el.end);
-                     console.log(el.work);
-                 },
-                 updateEvent: function (event) {
-                     console.log(event);
-                 },
-                 eventClick: function (data, jsEvent, view) {
-                     $scope.jobdescription = data.price;
-                     $scope.$apply();
-                     $("#eventContent").dialog({ modal: true, title: data.title, width: 350 });
-                 }
- 
-             });*/
-
 
             // Cut FROM Here
 
@@ -449,11 +235,6 @@
             }
         },
 
-        //link: function (s) {
-        //    window.ets = s;
-        //    s.onMouseHoverJob = function () {
-        //        s.setAlert('Deleting tree', { busy: true, time: "false" });
-        //    };
-        //}
+      
     }
 });

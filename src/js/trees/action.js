3d3004607c30bb8372b547998c52c9140a0b8fac
root <info@vyew.com>
2015-04-28 18:08:56 -0700

    ADD: customer est view - show link to view the basic page

--- src/js/trees/action.js
+++ src/js/trees/action.js
@@ -1,6 +1,19 @@
 var ActionCtrl = app.controller('ActionCtrl', ['$scope', '$location', function (s, $location) {
 	'use strict';
 
+	// set "basic page" link
+	var lnk=window.location.hash;
+	var hr=jQuery('#basic_pg_link');
+	if(lnk && hr){ 
+		// hide if this is not an estimate link
+		if(!lnk.match(/\/estimate\//)) jQuery("#basicAlert").hide();
+
+		// change link from #/estimate/XXX ---> to ---> /go/estimate/XXX
+		if(lnk.substr(0,1)=='#') lnk=lnk.substr(1);
+		lnk='/go'+lnk;
+		hr.attr('href',lnk);
+	}
+
 	setTimeout(function(){
 		$('#action-container').affix({
 			offset: { top: $('#action-container').offset().top }

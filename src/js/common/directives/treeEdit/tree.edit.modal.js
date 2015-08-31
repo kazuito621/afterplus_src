73d2358f6614e31533269b197abb0c90f117120a
tim <info@vyew.com>
2015-08-31 01:42:23 -0700

    fix carosel images in tree edit modal - wasnt displaying if only one image... and fix width/height ratio

--- src/js/common/directives/treeEdit/tree.edit.modal.js
+++ src/js/common/directives/treeEdit/tree.edit.modal.js
@@ -65,20 +65,21 @@ app.directive('treeEditModal', ['$modal','Api', '$location', function ($modal,Ap
             s.slides6 = [];
             s.carouselIndex6 = 1;
             s.tree.mode = mode;
+				console.debug("init ...  ");
             s.colors = ["#fc0003", "#f70008", "#f2000d", "#ed0012", "#e80017", "#e3001c", "#de0021", "#d90026", "#d4002b", "#cf0030", "#c90036", "#c4003b", "#bf0040", "#ba0045", "#b5004a", "#b0004f", "#ab0054", "#a60059", "#a1005e", "#9c0063", "#960069", "#91006e", "#8c0073", "#870078", "#82007d", "#7d0082", "#780087", "#73008c", "#6e0091", "#690096", "#63009c", "#5e00a1", "#5900a6", "#5400ab", "#4f00b0", "#4a00b5", "#4500ba", "#4000bf", "#3b00c4", "#3600c9", "#3000cf", "#2b00d4", "#2600d9", "#2100de", "#1c00e3", "#1700e8", "#1200ed", "#0d00f2", "#0800f7", "#0300fc"];
             var index=0;
+				console.debug(s.tree.images);
             _.each(s.tree.images,function(i){ // TODO after api change; use data.images instead dummyData.images
                 index++;
-                s.slides6.push(
-                    {
+              	var iData = {
                         id: (index),
                         label: 'slide #' + (index),
-                        img: i.imgMed ,  // TODO after api change; use data.imagePrefix instead dummyData.imagePrefix
-								link: i.imgLrg,
+                        img: i.imgMed + s.tree_cachebuster, 
+								link: i.imgLrg + s.tree_cachebuster,
                         color: s.colors[ (index*10) % s.colors.length],
                         odd: (index % 2 === 0)
-                    }
-                )
+                    };
+                s.slides6.push(iData);
             })
 
             // setup ESCAPE key

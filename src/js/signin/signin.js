4d7bb9f637fd9288f6042a373d33215d3d670e23
tim <info@vyew.com>
2015-11-10 16:12:31 -0800

    remove bad code causing redirect from estimates

--- src/js/signin/signin.js
+++ src/js/signin/signin.js
@@ -13,13 +13,6 @@ function ($scope, $timeout, $route, md5, $location, Auth, storage ){
 
 	var q=$location.search()
 
-	if(q.redirect && q.redirect.match(/estimate/)){
-		// todo.. this is not right
-		Auth.data({sessionID:'111'});
-		$location.url(q.redirect);
-		return;
-	}
-
 	//check for a token, and sign them in
 	var token = $location.path().substr(1).split("/");
 	if(token && token[1]){

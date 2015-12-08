5120fb2b5c8a3db21e4e3566e426cf90de144f2b
Nikita Muratov <muratov.nikita@gmail.com>
2015-12-08 11:41:14 +0100

    Formatting directives.js

--- src/js/directives.js
+++ src/js/directives.js
@@ -1,35 +1,36 @@
-
 //todo... this doesnt owrk. ESC to clear something
 // see :https://stackoverflow.com/questions/17470790/how-to-use-a-keypress-event-in-angularjs
 /*
-app.directive('ngEscape', function(){
-	return function(scope, element, attrs){
-dbg(attrs,'attrs')
-		element.bind("keydown keypress", function(event){
-dbg(event,'evt')
-			if(event.which===27){	//escape key
-dbg('got esc',attrs.ngEscape)
-				scope.$apply(function(){
-					var m = scope.$eval(attrs.ngModel);
-					m='';
-					scope.$eval(attrs.ngEscape);
-				});
-				//event.preventDefault();
-			}
-		});
-	}
+app.directive('ngEscape', function () {
+  'use strict';
+  return function (scope, element, attrs) {
+    dbg(attrs, 'attrs');
+    element.bind("keydown keypress", function (event) {
+      dbg(event, 'evt');
+      if (event.which === 27) { //escape key
+        dbg('got esc', attrs.ngEscape);
+        scope.$apply(function () {
+          var m = scope.$eval(attrs.ngModel);
+          m = '';
+          scope.$eval(attrs.ngEscape);
+        });
+        //event.preventDefault();
+      }
+    });
+  };
 });
 */
 
 
 // set focus on input field
-app.directive('autoFocus', function($timeout) {
-    return {
-        restrict: 'AC',
-        link: function(_scope, _element) {
-            $timeout(function(){
-                _element[0].focus();
-            }, 0);
-        }
-    };
+app.directive('autoFocus', function ($timeout) {
+  'use strict';
+  return {
+    restrict: 'AC',
+    link: function (_scope, _element) {
+      $timeout(function () {
+        _element[0].focus();
+      }, 0);
+    }
+  };
 });

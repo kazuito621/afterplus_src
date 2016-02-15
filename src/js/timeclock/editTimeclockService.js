f274b670131c671753a64c64b5e8aeee41cfd558
Andrei Bryk <andrei.bryk@gmail.com>
2016-02-15 21:44:43 +0300

    time editor improvement

--- src/js/timeclock/editTimeclockService.js
+++ src/js/timeclock/editTimeclockService.js
@@ -8,7 +8,7 @@ app
             scope.users = users;
             scope.usersFirstNames = _.pluck(users, 'fName').join(', ');
 
-            scope.events = TimeclockService.transformSchedule(_.first(users).schedule);
+            scope.events = _.first(users).schedule;
             console.log(scope.events);
 
             editTimeclockModal = $modal({
@@ -18,6 +18,94 @@ app
             });
 
             editTimeclockModal.$promise.then(editTimeclockModal.show);
+        };
+
+        scope.editTime = function (event) {
+            var changedIndex = _.indexOf(scope.events, event);
+
+            var timeStartMoment = moment(new Date(Date.parse(event.time)));
+            var timeStartOriginalMoment = moment(new Date(Date.parse(event.time_original)));
+
+            var adjustment = TimeclockService.msToHM(moment(timeStartMoment.diff(timeStartOriginalMoment)));
+            var adjustments = adjustment.split(':');
+
+            event.time_original = event.time;
+
+            for (var i = changedIndex - 1; i > 0; i--) {
+                var newDate = new Date(Date.parse(scope.events[i].duration));
+
+                newDate.setHours(newDate.getHours() + parseInt(adjustments[0]));
+                newDate.setMinutes(newDate.getMinutes() + parseInt(adjustments[1]));
+
+                scope.events[i].duration = newDate;
+                scope.events[i].original_duration = newDate;
+            }
+
+            for (var i = changedIndex + 1; i < scope.events.length; i ++) {
+                var newDate = new Date(Date.parse(scope.events[i].time));
+                var newDateEnd = new Date(Date.parse(scope.events[i].time_end));
+
+                newDate.setHours(newDate.getHours() + parseInt(adjustments[0]));
+                newDate.setMinutes(newDate.getMinutes() + parseInt(adjustments[1]));
+                newDateEnd.setHours(newDateEnd.getHours() + parseInt(adjustments[0]));
+                newDateEnd.setMinutes(newDateEnd.getMinutes() + parseInt(adjustments[1]));
+
+                scope.events[i].time = newDate;
+                scope.events[i].time_end = newDateEnd;
+                scope.events[i].original_time = newDate;
+            }
+
+        };
+
+        scope.editDuration = function (event) {
+            var changedIndex = _.indexOf(scope.events, event);
+
+            var timeStartMoment = moment(new Date(Date.parse(event.duration)));
+            var timeStartOriginalMoment = moment(new Date(Date.parse(event.duration_original)));
+
+            var adjustment = TimeclockService.msToHM(moment(timeStartMoment.diff(timeStartOriginalMoment)));
+            var adjustments = adjustment.split(':');
+
+            event.duration_original = event.duration;
+
+            for (var i = changedIndex + 1; i < scope.events.length; i ++) {
+                var newDate = new Date(Date.parse(scope.events[i].time));
+                var newDateEnd = new Date(Date.parse(scope.events[i].time_end));
+
+                newDate.setHours(newDate.getHours() + parseInt(adjustments[0]));
+                newDate.setMinutes(newDate.getMinutes() + parseInt(adjustments[1]));
+                newDateEnd.setHours(newDateEnd.getHours() + parseInt(adjustments[0]));
+                newDateEnd.setMinutes(newDateEnd.getMinutes() + parseInt(adjustments[1]));
+
+
+                scope.events[i].time = newDate;
+                scope.events[i].time_end = newDateEnd;
+                scope.events[i].original_time = newDate;
+            }
+
+        };
+
+        scope.removeBreak = function (event) {
+            var changedIndex = _.indexOf(scope.events, event);
+
+            var prevWork = scope.events[changedIndex - 1];
+            var nextWork = scope.events[changedIndex + 1];
+
+            console.log(prevWork.time);
+            console.log(nextWork.time);
+
+            var newIndex = changedIndex-1;
+            var newWorkEvent = TimeclockService.createEvent('work', prevWork.time, nextWork.time_end)
+
+            scope.events.splice(changedIndex+1,1);
+            scope.events.splice(changedIndex,1);
+            scope.events.splice(changedIndex-1,1);
+
+            scope.events.splice(newIndex, 0, newWorkEvent);
+        };
+
+        scope.addBreak = function (event) {
+
         };
 
         var showModal = function (users) {
--- src/js/timeclock/timeclock.js
+++ src/js/timeclock/timeclock.js
@@ -18,9 +18,9 @@ function TimeclockController (TimeclockService, editTimeclockService) {
         vm.haveSelectedUsers = true;
         if (selectedUser.selected) {
             var otherSchedule = _.filter(vm.users, function(user) {
-                return user.clockinby_userID != selectedUser.clockinby_userID;
+                return !TimeclockService.haveSimilarSchedule(user, selectedUser);
             });
-            
+
             _.each(otherSchedule, function (user) {
                 user.disabled = true;
             });
@@ -46,7 +46,10 @@ TimeclockService.$inject = ['$q', 'Api']
 function TimeclockService($q, Api) {
     var service = {
         getUsers: getUsers,
-        transformSchedule: transformSchedule
+        haveSimilarSchedule: haveSimilarSchedule,
+        transformSchedule: transformSchedule,
+        createEvent: createEvent,
+        msToHM: msToHM
     };
 
     return service;
@@ -95,6 +98,7 @@ function TimeclockService($q, Api) {
             });
 
             users[i]['schedule'] = _.where(schedules, { 'userID': user.userID });
+            users[i]['schedule'] = transformSchedule(users[i]['schedule']);
         });
 
         return users;
@@ -107,23 +111,17 @@ function TimeclockService($q, Api) {
                     "userID":3,
                     "reportID":2470,
                     "time_in":"2015-02-10 06:00:00",
-                    "time_out":"2015-02-10 10:00:00"
-                },
-                {
-                    "userID":17,
-                    "reportID":2470,
-                    "time_in":"2015-02-10 06:00:00",
-                    "time_out":"2015-02-10 10:00:00"
+                    "time_out":"2015-02-10 07:30:00"
                 },
                 {
                     "userID":3,
                     "reportID":2470,
-                    "time_in":"2015-02-10 16:00:00",
-                    "time_out":"2015-02-10 20:00:00"
+                    "time_in":"2015-02-10 08:00:00",
+                    "time_out":"2015-02-10 10:00:00"
                 },
                 {
-                    "userID":17,
-                    "reportID":2470,
+                    "userID":3,
+                    "reportID":2469,
                     "time_in":"2015-02-10 16:00:00",
                     "time_out":"2015-02-10 20:00:00"
                 }
@@ -133,34 +131,85 @@ function TimeclockService($q, Api) {
         return mockData.data;
     };
 
+    function haveSimilarSchedule(user, selectedUser) {
+        return isEqualSchedule(user.schedule, selectedUser.schedule);
+    };
+
+    function isEqualSchedule(schedule, other) {
+        if (schedule.length != other.length) {
+            return false;
+        }
+
+        _.each(schedule, function (event, i) {
+            if (
+                schedule[i].type != other[i].type ||
+                schedule[i].time != other[i].time
+            ) {
+                return false;
+            }
+        });
+
+        return true;
+    };
+
     function transformSchedule(schedule) {
         var events = [];
-        
+
         _.each(schedule, function (scheduleEntry, i) {
+
             if (schedule[i-1] != undefined) {
-                switchEvent = {};
-                switchEvent.type = 'switch';
-                events.push(switchEvent);
+                if (schedule[i-1].reportID != schedule[i].reportID) {
+                    events.push(createEvent('switch', scheduleEntry.time_in, scheduleEntry.time_out))
+                } else {
+                    events.push(createEvent('pause', schedule[i-1].time_out, scheduleEntry.time_out))
+                }
             } else {
-                startEvent = {};
-                startEvent.type = 'start';
-                events.push(startEvent);
+                events.push(createEvent('start', scheduleEntry.time_in, scheduleEntry.time_out))
             }
 
-            workEvent = {};
-            workEvent.type = 'work';
-            events.push(workEvent);
+            events.push(createEvent('work', scheduleEntry.time_in, scheduleEntry.time_out))
 
 
-            if (schedule[i+1] != undefined) {
-
-            } else {
-                stopEvent = {};
-                stopEvent.type = 'stop';
-                events.push(stopEvent);
+            if (schedule[i+1] == undefined) {
+                events.push(createEvent('stop', scheduleEntry.time_out));
             }
-        })
-
+        });
+        console.log(events);
         return events;
     };
+
+    function createEvent(type, timeStart, timeEnd) {
+        timeStartMoment = moment(timeStart, 'YYYY-MM-DD HH:mm:ss');
+
+        if (timeEnd != null) {
+
+            timeEndMoment = moment(timeEnd, 'YYYY-MM-DD HH:mm:ss');
+            duration = moment(timeEndMoment.diff(timeStartMoment));
+        }
+
+        var event = {};
+        event.type = type;
+        event.time = timeStartMoment.format('YYYY-MM-DD HH:mm:ss');
+
+        if (timeEnd != null) {
+            event.time_end = timeEnd;
+            event.duration = moment(msToHM(duration), 'H:m').format('YYYY-MM-DD HH:mm:ss');
+        }
+
+
+        event.time_original = event.time;
+        event.duration_original = event.duration;
+        event.time_end_original = event.time_end;
+
+        return event;
+    }
+
+    function msToHM( ms ) {
+        var seconds = ms / 1000;
+        var hours = parseInt( seconds / 3600 );
+        seconds = seconds % 3600;
+        var minutes = parseInt( seconds / 60 );
+        seconds = seconds % 60;
+        return hours+':'+minutes;
+    }
 };
